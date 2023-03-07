335374fce28659bd315ceca57d202e1e5ac907ad
Erik Cardenas <efdree@gmail.com>
2023-03-07 14:51:21 -0500

    Fix: SeachEmployee

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,9 @@
 import styled from "@emotion/styled";
 import logo from "../assets/logo.png";
 import { Link } from "react-router-dom";
-import Input from "./Input";
 import { useState, } from "react";
 import { useNavigate } from "react-router-dom";
-import debounce from "";
+import {DebounceInput} from 'react-debounce-input';
 
 const ContainHeader = styled.div`
   background: #f2f2f2;
@@ -18,6 +17,14 @@ const Brand = styled.img`
   height: 25px;
 `;
 
+const InputSearch = {
+  borderRadius: "6px",
+  padding: "8px 12px",
+  background: "#FFFFFF",
+  border: "1px solid #D3D3D3",
+  boxShadow: "0px 1px 2pp rgba(0, 0, 0, 0.05)",
+  width: "100%"
+}
 function Header() {
   const [query, setQuery] = useState("");
 
@@ -30,18 +37,18 @@ function Header() {
     navigate(`/lookforEmployees/${data}`);
   }
 
-
-
   return (
       <ContainHeader>
       <Link to="/">
           <Brand src={logo} alt="logo" />
         </Link>
-        <Input
+        <DebounceInput
           placeholder={"search"}
           name="query"
           value={query}
+          debounceTimeout={300}
           onChange={handleChange}
+          style={InputSearch}
         />
       </ContainHeader>
   );
